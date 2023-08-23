12419ddfd46e17c9054354edc596bb6b0bccc8da
ThickDepression <yami.onlyme@gmail.com>
2023-08-23 19:09:27 +0100

    add

--- pchar.c
+++ pchar.c
@@ -12,7 +12,7 @@ void pchar(stack_t **head, unsigned int lNum)
 
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%d: can't pchar, an empty stack\n", lNum);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", lNum);
 		fclose(infos.file);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
